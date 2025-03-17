d03cf47f78defc8fe6f6ec69f82b0c1f61efab22
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-03-17 22:44:40 +0530

    is is working properly:

---
+++ static/js/cart.js
@@ -0,0 +1,20 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i <updateBtns.length; i++){
+
+    updateBtns[i].addEventListener('click', function(){
+        var productID = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:',productID, 'action:',action)
+
+
+        console.log('USER:', user)
+
+        if(USER ==='Anonymous User'){
+            console.log('Not logged in')
+
+        }else{
+            console.log("User is logged in, seding data")
+        }
+    })
+}
