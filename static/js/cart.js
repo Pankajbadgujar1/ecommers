c122704b33e54bea0386bdac88f35d7dcb8e343d
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-03-18 18:47:09 +0530

    a

--- static/js/cart.js
+++ static/js/cart.js
@@ -20,6 +20,39 @@ for(var i = 0; i <updateBtns.length; i++){
         }
     })
 }
+
+
+function addCookieItem(productId, action){
+	
+	console.log("Not loogged in..")
+
+	console.log("user is not authenticated ")
+	if (action == 'add'){
+		if(cart[productId] == undefined){
+			cart[productId]= {'quantity':1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+		
+	}
+
+
+	if (action == 'remove'){
+		
+		cart[productId]['quantity'] -= 1
+
+		if(cart[productId]['quantity'] <= 0){
+			console.log('Remove Items')
+			delete cart[productId]
+
+		}
+		console.log("cart:",cart)
+		document.cookie = 'cart =' + JSON.stringify(cart) + ";domain=;path=/"
+		location.reload()
+	}
+}
+
+
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
 
--- store/views.py
+++ store/views.py
@@ -29,12 +29,15 @@ def cart(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
+		cart = json.loads(request.COOKIES['cart'])
+		print("cart",cart)
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-
-	context = {'items':items, 'order':order}
+		cartItems = order.get_cart_items
+	context = {'items':items, 'order':order , 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
 
